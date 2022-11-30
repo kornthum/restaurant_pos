145e2f68c716723d019ffb3fe337da9d780cc30b
kornthum <Kornthum@hotmail.com>
2022-11-30 23:41:14 +0700

    edit streaming transform

--- spark/transform_pos_streaming.py
+++ spark/transform_pos_streaming.py
@@ -8,7 +8,7 @@ from pyspark.sql.types import *
 spark = SparkSession.builder.appName("strm_spark_job").enableHiveSupport().getOrCreate()
 spark.conf.set("spark.sql.streaming.checkpointLocation", "/tmp/checkpoints")
 
-userSchema = StructType().add("customer_id", "integer").add("restaurant_name", "str").add("customer_order", "string").add("order_timestamp", "integer").add("pos_type", "str")
+userSchema = StructType().add("id", "integer").add("restaurant_name", "str").add("order", "string").add("order_timestamp", "integer").add("pos_type", "str")
 
 strmDF = spark \
     .readStream \
@@ -16,7 +16,7 @@ strmDF = spark \
     .option("sep", "|") \
     .csv("/tmp/flume/sink")
 
-split_col = split(strmDF['customer_order'], ' ')
+split_col = split(strmDF['order'], ' ')
 strmDF = strmDF.withColumn('order_menu', split_col.getItem(0)) \
 .withColumn('order_price', split_col.getItem(1).cast('integer')) \
 .withColumn("order_timestamp",from_unixtime(col("order_timestamp"),'dd-MM-yyyy HH:mm:ss').cast('string')) \
@@ -24,7 +24,7 @@ strmDF = strmDF.withColumn('order_menu', split_col.getItem(0)) \
 
 
 query = strmDF \
-    .selectExpr('customer_id as cust_id', 'restaurant_name as res_name','order_menu as odr_menu','order_price as odr_prc','order_timestamp as odr_tms', 'pos_type as pos_type') \
+    .selectExpr('id', 'restaurant_name','order_menu','order_price','order_timestamp', 'pos_type') \
     .writeStream \
     .outputMode("append") \
     .format("parquet") \
