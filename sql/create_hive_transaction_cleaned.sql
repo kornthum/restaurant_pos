3f70c61e0cb3bfd889fc01fd96160a7813ca25c3
kornthum <Kornthum@hotmail.com>
2022-12-01 00:46:05 +0700

    update transaction_clean sql

--- sql/create_hive_transaction_cleaned.sql
+++ sql/create_hive_transaction_cleaned.sql
@@ -1,16 +1,9 @@
-CREATE TABLE default.transactions(
+CREATE EXTERNAL TABLE transactions_cln(
    customer_id         int
-   restaurant_name  string
+  ,restaurant_name  string
   ,customer_order  	string
   ,order_timestamp	bigint
   ,pos_type         string
 ) 
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY '|' 
-LINES TERMINATED BY '\n' 
-STORED AS INPUTFORMAT 
-'org.apache.hadoop.mapred.TextInputFormat' 
-OUTPUTFORMAT 
-'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION
-'/tmp/flume/sink/'
+STORED AS PARQUET
+LOCATION '/tmp/default/transactions_cln/'
