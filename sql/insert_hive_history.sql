bbf68a1dee77aa8a440bf27353b59b55032e0871
kornthum <Kornthum@hotmail.com>
2022-12-01 01:29:41 +0700

    un partition

--- sql/insert_hive_history.sql
+++ sql/insert_hive_history.sql
@@ -1,5 +1,4 @@
 INSERT OVERWRITE TABLE history
-PARTITION(data_dt='${data_dt}')
 select res.restaurant_name, trans.order_menu, sum(trans.odr_prc) as spend_amount 
 from restaurant as res
 join transactions_cln as trans
